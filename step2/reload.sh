3d9b161e21071877c417873d8cec4f0f8b6b503f
galedric <g@ledric.me>
2016-06-01 14:20:45 +0200

    Step 5

--- step2/reload.sh
+++ step2/reload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
 
 docker build -t http/step2 .
 docker run -d -p 8080:80 http/step2
--- step3/reload.sh
+++ step3/reload.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
 
 docker build -t http/step3 .
 
-docker run -d http/step1
-docker run -d http/step2
-docker run -d -p 80:80 http/step3
+docker run -d --name dynamic http/step2
+docker run -d --name static http/step1
+docker run -d -p 80:80 --link static:static --link dynamic:dynamic http/step3
 
